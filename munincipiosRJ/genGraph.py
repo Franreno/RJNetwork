075943e990b291641745651014826b8d8feec43a
Franreno <franreno4@gmail.com>
2021-10-06 08:44:17 -0300

    Adding Plotly network

--- munincipiosRJ/genGraph.py
+++ munincipiosRJ/genGraph.py
@@ -17,6 +17,15 @@ def createNetwork():
     for i in range(len(data)):
         G.add_nodes_from([(i, data[i][1])])
 
+    # Generate random positions
+    pos = nx.drawing.layout.random_layout(G)
+    # Add to the nodes data
+
+    i = 0
+    for node in G.nodes:
+        G.nodes[node]['gpos'] = pos[i]
+        i += 1
+
     # Add edges
     for i in range(len(data)):
         cityNumber = data[i][0]
@@ -34,6 +43,6 @@ def createNetwork():
     return G
 
 
-G = createNetwork()
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.show()
+# G = createNetwork()
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
---
+++ munincipiosRJ/test.py
@@ -0,0 +1,74 @@
+from genGraph import createNetwork
+import networkx as nx
+import plotly.graph_objects as go
+
+G = createNetwork()
+
+
+edge_x = []
+edge_y = []
+for edge in G.edges():
+    x0, y0 = G.nodes[edge[0]]['gpos']
+    x1, y1 = G.nodes[edge[1]]['gpos']
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+
+edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+node_x = []
+node_y = []
+for node in G.nodes():
+    x, y = G.nodes[node]['gpos']
+    node_x.append(x)
+    node_y.append(y)
+
+node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        # colorscale options
+        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2))
+
+node_adjacencies = []
+node_text = []
+for node, adjacencies in enumerate(G.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+    node_text.append(f"Municipio: {G.nodes[node]['Municipio']}[{str(node)}]   |   Numero de conexoes: {str(len(adjacencies[1]))}")
+
+node_trace.marker.color = node_adjacencies
+node_trace.text = node_text
+
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Grafo Rio de Janeiro',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+fig.show()
