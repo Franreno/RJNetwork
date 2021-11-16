b62251a102edc201b6654c551f90b16ca1972c9a
Franreno <franreno4@gmail.com>
2021-11-16 09:49:53 -0300

    Uploading Moving average plots

--- Analise_GeoPandas/plotlyGeoPandas.py
+++ Analise_GeoPandas/plotlyGeoPandas.py
@@ -5,7 +5,8 @@ import json
 
 rjPath = "./RJdata/RJ.json"
 weeklyDengueDataPath = "./RJdata/Dengue_Brasil_2010-2016_Daniel.xlsx"
-outputPath = "./plotlyPages/paginasGeoJSON/"
+dadosMunicipaisPath = "./RJdata/dadosMunicipiosRJ.xlsx"
+
 
 names = ['id', 'cities']
 for i in range(1, 53):
@@ -26,12 +27,11 @@ print("Aberto com sucesso")
 cols = ["Municipio", "Week", "Value"]
 years = ["2010", "2011", "2012", "2013", "2014", "2015"]
 
-for year in years:
-    print(f"\nStatus: Ano {year}\n")
 
-    dataDF = weeklyDengueData[year]
+dadosMunicipais = pd.read_excel(dadosMunicipaisPath)
+dadosMunicipais = dadosMunicipais.rename(columns={'População_estimada_pessoas': 'Pop'})
 
-    print("Criando frames")
+def createWeeklyData(dataDF):
     mainDataList = []
     for i in range(len(dataDF["cities"])):
         cumulative = 0
@@ -39,7 +39,66 @@ for year in years:
             cumulative += dataDF[str(week)][i]
             dataList = [dataDF["cities"][i], week, cumulative]
             mainDataList.append(dataList)
+    return mainDataList
+
+def createMediaMovelSimples(dataDF):
+    mainDataList = []
+    for i in range(len(dataDF["cities"])):
+        j=0
+        month=0
+        while(j < 52):
+            cumulative = 0
+            
+            while(j % 4 != 0 or j == 0):
+                cumulative += dataDF[str(weeks[j])][i]
+                j+=1
+            
+            cumulative /= 4
+            dataList = [dataDF["cities"][i], month, cumulative]
+            mainDataList.append(dataList)
+            month+=1
+            j+=1
+        
+    cols[1] = "Month"
+
+    return mainDataList
+
+
+def createMediaMovelPonderada(dataDF, outputPath, titleStr):
+    pass
+
+for year in years:
+    print(f"\nStatus: Ano {year}\n")
+
+    dataDF = weeklyDengueData[year].join(dadosMunicipais["Pop"])
+
+    typeIndex = 2
+
+    titleStr = [f"Casos de Dengue por semana em {year}",
+                f"Casos de Dengue por semana com população em {year}" 
+                f"Casos de Dengue média móvel em {year}",
+                f"Casos de Dengue média móvel com população em {year}"]
+
+    outputPath = [
+        "./plotlyPages/paginasGeoJSON/byWeeks/",
+        "./plotlyPages/paginasGeoJSON/byWeeksWithPopulation/",
+        "./plotlyPages/paginasGeoJSON/bySimpleMovingAverage/",
+        "./plotlyPages/paginasGeoJSON/bySimpleMovingAverageWithPopulation/"
+    ]
+
+    functions = [
+        createWeeklyData(dataDF),
+        createWeeklyData(dataDF),
+        # createWeeklyDataWithPopulation(dataDF),
+        createMediaMovelSimples(dataDF), 
+        # createMediaMovelSimplesWithPopulation(dataDF), 
+    ]
+
+    print("Criando frames")
+    # mainDataList = createWeeklyData(dataDF)
+    mainDataList = functions[typeIndex]
     print("Criado com sucesso")
+    
 
     finalDF = pd.DataFrame(mainDataList, columns=cols)
 
@@ -50,9 +109,9 @@ for year in years:
         geojson=mp,
         featureidkey="properties.NOME",
         locationmode='geojson-id',
-        animation_frame="Week",
+        animation_frame=cols[1],
         color="Value",
-        title=f"<b> Casos de Dengue por semana em {year}<b>"
+        title=f"{titleStr[typeIndex]}"
     )
     print("Figura criada")
 
@@ -63,5 +122,5 @@ for year in years:
 
     print("Salvando figura")
     fig.show()
-    fig.write_html(outputPath + year + '.html')
+    fig.write_html(outputPath[typeIndex] + year + '.html')
     print("Sucesso...")
