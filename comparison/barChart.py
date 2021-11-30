1464e835d14cccc9f2fc88a425b632f21bc7319d
Franreno <franreno4@gmail.com>
2021-11-30 17:19:02 -0300

    Final barChart

--- comparison/barChart.py
+++ comparison/barChart.py
@@ -12,7 +12,7 @@ years = ["2010", "2011", "2012"]
 
 cities = list(IBGEDICT["2010"].keys())
 
-fig = make_subplots(rows=92, cols=1)
+fig = make_subplots(rows=92, cols=1, subplot_titles=cities)
 
 for i in range(len(cities)):
     city = cities[i]
@@ -23,9 +23,14 @@ for i in range(len(cities)):
         ibge.append(IBGEDICT[year][city])
         ufrj.append(UFRJDICT[year][city])
 
-    fig.add_trace(go.Bar(x=years, y=ufrj), row=i+1, col=1)
-    fig.add_trace(go.Bar(x=years, y=ibge), row=i+1, col=1)
+    fig.add_trace(go.Bar(x=years, y=ufrj, name="UFRJ", text=ufrj), row=i+1, col=1)
+    fig.add_trace(go.Bar(x=years, y=ibge, name="IBGE", text=ibge), row=i+1, col=1)
 
-fig['layout'].update(showlegend=False, height=10000, width=1200)
+fig['layout'].update(showlegend=False, height=25000, width=1200)
 
+
+outputPath = "./plotlyPages/comparision/"
+print("Salvando figura")
 fig.show()
+fig.write_html(outputPath + "Bar" + '.html')
+print("Sucesso...")
